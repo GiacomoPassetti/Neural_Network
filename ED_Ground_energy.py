98812c525cf985c5e9c2e374f2d7be9e892e6f02
Jack <giacomopassetti@hotmail.it>
2021-11-30 13:55:28 +0100

    written main for groundstate ED plot

--- ED_Ground_energy.py
+++ ED_Ground_energy.py
@@ -1,9 +1,13 @@
 from functions import Sparse_SYK
 from scipy.sparse.linalg import eigsh
-L = 6
-N = 3
+import numpy as np
+
 J = 1
-u, v = eigsh(Sparse_SYK(L, N, J), 1, which = 'SA')
-print(u)
 
+energies = []
+for L in list(np.arange(4, 22, 2)):
+    N = int(L/2)
+    u, v = eigsh(Sparse_SYK(L, N, J), 1, which = 'SA')
+    energies.append(u[0])
 
+np.save("Ground_state_syk_single_IT.npy", energies )
