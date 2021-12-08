Added the correct numerical prefactor in the function H_syk_dumb, now the energy still converge to a lower value than the ED_gs but is currently around a factor 10*N (roughly)

calc_trans_states.py
@@ -104,13 +104,13 @@ def seed_matrix(states, transitions):
         
         return trans_converted
 
-def dumb_syk_transitions( seed_matrix, seed ):
+def dumb_syk_transitions( seed_matrix, seed, L ):
     H_syk = torch.zeros(seed_matrix.shape)
     for i in range(seed_matrix.shape[0]):
         for j in range(seed_matrix.shape[1]):
             np.random.seed(seed_matrix[i, j]*seed)
             
-            H_syk[i, j] = np.random.normal(0, 1)
+            H_syk[i, j] = np.random.normal(0, 1)*(4/((2*L)**(3/2)))
             
     return H_syk
 

main_with_batch.py
@@ -11,7 +11,7 @@ seed = 1
 
 #region Parameters
 # Physical parameters of the SYK Model
-L = 4
+L = 10
 N = int(L/2)
 J = 1
 
@@ -43,7 +43,7 @@ trans_states = trans_unique(trans_states)
 print(trans_states.shape)
 
 
-syk = dumb_syk_transitions(seed_matrix(input_states, trans_states), seed)
+syk = dumb_syk_transitions(seed_matrix(input_states, trans_states), seed, L)
 
 trans_states = torch.transpose(trans_states, 1, 2)
 Net = seq_modules(L, net_dim, layers)